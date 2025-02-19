43d0756f4d2100ddb41c371c74866b592f441f1d
Ian Souza Freire <iantmessenger+@gmail.com>
2025-02-19 19:40:02 -0300

    take out pay

--- index.js
+++ index.js
@@ -19,24 +19,16 @@ function currentAge(year, month, day) {
   if (monthDifference < 0 || (monthDifference === 0 && currentDate.getDate() < birthDate.getDate())) {
     age--;
   }
+
+  if (currentAge.getMonth() < month) {
+    age++;
+  }
   
   return document.createTextNode(age);
 }
 
 $(document).ready(() => {
   $('#age').html(currentAge(2008, 7, 3));
-
-  const pixCanvas = Pix("iantmessenger@gmail.com", "IAN SOUZA FREIRE", "GOIÁS", $('#valor-pix').val(), "Donation", true);
-
-  $('#valor-pix').on('keydown', () => {
-    if (pixCanvas instanceof HTMLCanvasElement) {
-      const qrCodeDataUrl = pixCanvas.toDataURL();
-        
-      $('#qr-code').attr('src', qrCodeDataUrl);
-    } else {
-      console.error("Pix function did not return a valid canvas element.");
-    }
-  })
 
   // Animate elements on scroll
   gsap.registerPlugin(ScrollTrigger);
